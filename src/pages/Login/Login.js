ea20443e02fc51dc38b292822b84f1a1053cbf08
AYUSH PATNI <ayushdpatni@gmail.com>
2022-08-27 23:33:29 +0530

    footer added in login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
 import './Login.css'
+import { GrLinkedin, GrInstagram, GrFacebook, GrGithub, GrTwitter } from "react-icons/gr";
 
 SwiperCore.use([EffectCoverflow, Pagination]);
 // if you want to use array
@@ -53,6 +54,47 @@ const Login = () => {
           );
         })}
       </Swiper>
+      <br/>
+      <br/>
+      <br/>
+      {/* ---------------footer-------------- */}
+    <footer className="footer">
+            <div className="footer__bg">
+                <div className="footer__container container grid">
+                    <div>
+                        <h1 className="footer__title">
+                          XYZ
+                        </h1>
+                        <span className="footer__subtitle">
+                            Group of Hostels.
+                        </span>
+                        < br/>
+                        < br/>
+                    </div>
+                    <div className="footer__socials">
+                            <a href="https://www.facebook.com/ayushpatni" target="_blank" className="footer__social" rel="noreferrer">
+                              <GrFacebook />
+                            </a>
+                            <a href="https://www.instagram.com/ayushpatni_22/" target="_blank" className="footer__social" rel="noreferrer">
+                              <GrInstagram />
+                            </a>
+                            <a href="https://www.twitter.com/ayushpatni_22" target="_blank" className="footer__social" rel="noreferrer">
+                              <GrTwitter />
+                            </a>
+                            <a href="https://www.github.com/ayushdpatni" target="_blank" className="footer__social" rel="noreferrer">
+                              <GrGithub />
+                            </a>
+                            <a href="https://www.linkedin.com/in/ayushpatni22/" target="_blank" rel="noreferrer"className="footer__social">
+                            <GrLinkedin />
+                            </a>
+                            
+                    </div>
+                </div>
+                <p className="footer__copy">&#169; XYZ Hostels. All right reserved.</p>
+                <script src="https://kit.fontawesome.com/9620e50583.js" crossorigin="anonymous"></script>
+            </div>
+        </footer>
+    
     </div>
   );
 };
